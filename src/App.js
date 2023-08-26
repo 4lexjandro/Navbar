d32ae6b608c8816874952b55cdc6c538497afc17
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2023-08-26 11:57:07 -0400

    navbar barebones done, just need to add functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Contact from './Routes/Contact'
+import About from './Routes/About'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <Router>
+      <Navbar/>
+
+      <Routes>
+    
+     <Route path='/about' element={<About />} />
+
+     <Route path='/contact' element={<Contact />} />
+    
+     </Routes>
+     <h1>hello</h1>
+
+    </Router>
+    </>
   );
 }
 
